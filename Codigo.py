6e6a6e45a2a306ab117033f0bd1c39a50ad1d321
cristian1938 <59376790+cristian1938@users.noreply.github.com>
2020-10-15 18:05:45 -0500

    Create Codigo.py
    añadi los primeros codigos

---
+++ Codigo.py
@@ -0,0 +1,26 @@
+
+
+import os
+os.environ['NUMBAPRO_LIBDEVICE'] = "/usr/local/cuda-10.0/nvvm/libdevice"
+os.environ['NUMBAPRO_NVVM'] = "/usr/local/cuda-10.0/nvvm/lib64/libnvvm.so"
+
+import numpy as np
+npoints = int(1e7)
+a = np.arange(npoints, dtype=np.float32)
+
+import math
+from numba import vectorize 
+
+@vectorize
+def cpu_sqrt(x):
+  return math.sqrt(x)
+
+cpu_sqrt(x)
+
+import math
+from numba import vectorize
+
+@vectorize(['float32(float32)'], target='cuda')
+def gpu_sqrt(x):
+    return math.sqrt(x)
+
